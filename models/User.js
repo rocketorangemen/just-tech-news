53aad39598583669799ff3eceeaeb430235a47f2
Mon <rocketorangemen@gmail.com>
2021-03-17 12:21:19 -0500

    save updates

--- models/User.js
+++ models/User.js
@@ -1,8 +1,14 @@
 const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
+const bcrypt = require('bcrypt');
 
 // create our User model
-class User extends Model {}
+class User extends Model {
+  // set up method to run on instance data (per user) to check password
+  checkPassword(loginPw) {
+    return bcrypt.compareSync(loginPw, this.password);
+  }
+}
 
 // create fields/columns for User model
 User.init(
@@ -34,12 +40,22 @@ User.init(
     }
   },
   {
+    hooks: {
+        //set up beforeCreate lifecycle "hook" functionality
+        async beforeCreate(newuserData) {
+          newuserData.password = await bcrypt.hash(newuserData.password, 10);
+          return newuserData;
+        },
+        async beforeUpdate(updatedUserData) {
+          updatedUserData.password = await bcrypt.hash(updatedUserData.password, 10);
+          return updatedUserData;
+    },
     sequelize,
     timestamps: false,
     freezeTableName: true,
     underscored: true,
-    modelName: 'user'
+    modelName: 'user',
   }
-);
+});
 
 module.exports = User;
